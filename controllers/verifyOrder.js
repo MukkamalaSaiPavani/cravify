95cf5c392e707aaa0b676332a7755ba13e1ffa08
Mukkamala Sai Pavani <pavani.23bce9863@vitapstudent.ac.in>
2025-07-02 15:43:02 +0530

    some fixes

---
+++ controllers/verifyOrder.js
@@ -0,0 +1,15 @@
+const verifyOrder = async (req, res) => {
+    const { orderId, success } = req.body;
+    try {
+        if (success === "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" });
+        } else {
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({ success: false, message: "Not Paid" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Not Verified" });
+    }
+};
